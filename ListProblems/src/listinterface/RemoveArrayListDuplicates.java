b80f524a63895ec97fdf5849080509a103a94679
Bhoomika-NV <bhoomikanv6@gmail.com>
2025-02-09 23:41:07 +0530

    reverse logic

--- ListProblems/src/listinterface/RemoveArrayListDuplicates.java
+++ ListProblems/src/listinterface/RemoveArrayListDuplicates.java
@@ -2,6 +2,8 @@ package listinterface;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /*Input: List = [1, 10, 2, 2, 10, 3, 3, 3, 4, 5, 5]
@@ -14,21 +16,29 @@ public class RemoveArrayListDuplicates {
 		stringList();
 		integerList();
 		reverseArrayList();
+		reverseUsingThe_reverseMethod();
+	}
+
+	private static void reverseUsingThe_reverseMethod() {
+         List<Integer> list= new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1));
+         Collections.reverse(list);
+         System.out.println(list);
 	}
 
 	private static void reverseArrayList() {
-		List<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 9, 0));
-		Integer start = 0;
-		Integer end = list.size() - 1;
-
-		while (start < end) {
-			int temp = list.get(start);
-			list.set(start, list.get(end));
-			list.set(end, temp);
-			start++;
-			end--;
-		}
-		System.out.println(list);
+		List<Integer> list = new ArrayList<Integer>(Arrays.asList(9,8,7,6,5,4,3,2,1));
+		//reverse list without another array using another array
+		int start=list.get(0);
+		int end=list.size()-1;
+      while(start<end) {
+    	  start=list.set(start,list.get(end));
+    	  end=list.set(start,list.get(start));
+    	  start++;
+    	  end--;
+      }
+      for(Integer i: list) {
+    	  System.out.print(i+" ");
+      }
 	}
 
 	private static void stringList() {
